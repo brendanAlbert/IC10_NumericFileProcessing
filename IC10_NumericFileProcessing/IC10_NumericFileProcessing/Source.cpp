f76deafb4650dc689b54fde5846199b323f262ff
brendanAlbert <alber123@mail.chapman.edu>
2016-03-07 17:44:33 -0800

    Added files via upload

---
+++ IC10_NumericFileProcessing/IC10_NumericFileProcessing/Source.cpp
@@ -0,0 +1,16 @@
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int number;
+	vector <int> allNumbers;
+
+	// To open a file for input:
+	ifstream randomFile("..\..\..\..\..\Desktop\Random.txt");
+
+	system("pause");
+	return 0;
+}
